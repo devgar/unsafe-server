8526e0430bccb1a23934460e0849e391f180f88c
Edgar Albalate <dev.gardo@gmail.com>
2020-12-11 15:38:42 +0100

    Switched to fastapi

--- unsafe_server/__init__.py
+++ unsafe_server/__init__.py
@@ -1,1 +1,10 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+
+import uvicorn
+
+app = "unsafe_server.main:app"
+
+
+def dev():
+    print("Running dev server")
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, workers=2)
